c463bf98951a9fe1c36651fc18d3c5df5fd75d41
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-05-27 14:37:57 +0530

    moved templates to sub directory

--- fromtemplate.ps1
+++ fromtemplate.ps1
@@ -1,6 +1,6 @@
 $filename = Read-Host -Prompt "Enter file name"
 $extension = Read-Host -Prompt "Enter file extension"
-$sourcePath = "C:\Users\pavan\Documents\Templates\template.$extension"
+$sourcePath = "C:\Users\pavan\Documents\PS-Scripts\templates\template.$extension"
 $destinationPath = Join-Path $PWD.Path "$filename.$extension"
 
 Copy-Item -Path $sourcePath -Destination $destinationPath -Force
